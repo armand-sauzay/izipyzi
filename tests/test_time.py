cd1deaefd24a21e78ef03499b4cd1322aa1eaad5
Armand Sauzay <35524799+armand-sauzay@users.noreply.github.com>
2024-06-13 21:25:03 +0200

    feat: initial repository setup (#1)

---
+++ tests/test_time.py
@@ -0,0 +1,22 @@
+from pybox.time import time_function_by_tries, time_function_by_duration
+import pytest
+
+
+@pytest.fixture
+def func():
+    def func():
+        return 1
+
+    return func
+
+
+def test_time_function_by_tries(func):
+    times = time_function_by_tries(func, tries=3)
+    assert len(times) == 3
+    assert all(isinstance(time, float) for time in times)
+
+
+def test_time_function_by_duration(func):
+    times = time_function_by_duration(func, duration=0.1)
+    assert all(isinstance(time, float) for time in times)
+    assert sum(times) > 0
